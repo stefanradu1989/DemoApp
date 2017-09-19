a43e960f9e8c87335957de50d7c4c34de0d5b4ed
Radu Stefan <radustefan@Radus-MacBook-Pro.local>
2017-09-19 14:16:42 +0300

    added classes for details screen

---
+++ DemoApp/DemoApp/Scenes/DetailScreen/DetailScreenViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailScreenViewController.swift
+//  DemoApp
+//
+//  Created by Radu Stefan on 19/09/2017.
+//  Copyright © 2017 RS. All rights reserved.
+//
+
+import UIKit
+
+class DetailScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ DemoApp/DemoApp/Scenes/DetailScreen/DetailScreenViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  DetailScreenViewModel.swift
+//  DemoApp
+//
+//  Created by Radu Stefan on 19/09/2017.
+//  Copyright © 2017 RS. All rights reserved.
+//
+
+import Foundation
+
+
+class DetailScreenViewModel {
+    
+    init() {
+        
+    }
+}
